9a58dcead5ff7bdc28a337eecb9db7cb729b41f8
rohit.kadam2 <rohit.kadam2@cipla.com>
2026-09-12 21:40:25 +0530

    changed cors policy

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const cors = require("cors");
 
 
 server.use(cors({
-  origin: ["http://localhost:5173", "https://sitbro.netlify.app"],
+  origin: "*",
   credentials:true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
